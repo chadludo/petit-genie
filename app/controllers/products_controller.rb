bd6d4c05edbd53eb138ada9bb331764d58d6cffd
Ludovic Villain <chadludo@gmail.com>
2021-04-01 16:10:00 +0200

    done jacadi scraping

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,9 +13,10 @@ class ProductsController < ApplicationController
 
   def create
     # Scraping method
-    @product = scrape || self
+    @product = scrape #|| add_product
+  end
 
-    # Classic create method
+  def add_product
     @list = List.find(params[:list_id])
     @product = Product.new(product_params)
     @product.list = @list
@@ -29,29 +30,25 @@ class ProductsController < ApplicationController
 
   def scrape
     # Create a product with scraping method for Jacadi brand
-    # 1 - Get the URL from form in create view
     @list = List.find(params[:list_id])
     @product = Product.new(product_params)
     @product.list = @list
     page_url = @product.url
-    # 2 - Parse the URL in nokogiri
     doc = Nokogiri::HTML(URI.open(page_url))
     # 3 - select all items with css selector
     items = doc.css(".j-wrapper-content")
     # 4 - Iterate on each item to save them
-    images = doc.css(".j-img")
-
-    images.each do |image|
-      link = image.css("img").attr('src').to_s
-      @product.image_url = "https://www.jacadi.fr#{link}"
-    end
-
     items.each do |item|
+      link = item.css(".j-img img").attr('src').to_s
+      @product.image_url = "https://www.jacadi.fr#{link}"
       @product.name = item.css("h1").text
       @product.price = item.css(".j-prd-price p").text.to_f
     end
-    # 5 - Save product in database
-    @product.save!
+    if @product.save!
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -61,14 +58,12 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     @product.update(product_params)
-
     redirect_to lists_path(@list)
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy!
-
     redirect_to lists_path(@list)
   end
 
